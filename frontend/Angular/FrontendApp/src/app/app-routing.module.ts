ae5c5a25df4d521334709cd379fc0a9852f0bcd7
Mireya <mireya.ndba@gmail.com>
2021-12-07 15:51:15 -0500

    Administracion modulo

--- frontend/Angular/FrontendApp/src/app/app-routing.module.ts
+++ frontend/Angular/FrontendApp/src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './templates/header/header.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+      path:"Inicio",
+      component: HeaderComponent,
+    },
+    {
+      path:"",
+      pathMatch:"full",
+      redirectTo:"/Inicio"
+    },
+    {
+      path:"administracion",
+      loadChildren: ()=>import("./modulos/administracion/administracion.module").then(x => x.AdministracionModule)
+    }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/Angular/FrontendApp/src/app/modulos/administracion/administracion-routing.module.ts
+++ frontend/Angular/FrontendApp/src/app/modulos/administracion/administracion-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BuscarPersonaComponent } from './personas/buscar-persona/buscar-persona.component';
+import { CrearPersonaComponent } from './personas/crear-persona/crear-persona.component';
+import { BuscarProductoComponent } from './productos/buscar-producto/buscar-producto.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"personas",
+    component:BuscarPersonaComponent
+  },
+  {
+    path:"productos",
+    component:BuscarProductoComponent
+  },
+  {
+    path:"personas/crear",
+    component:CrearPersonaComponent
+  },
+  {
+    path:"personas/buscar",
+    component:BuscarPersonaComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
