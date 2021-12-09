b92c04b31a4fa4bec0d10cadc7eb0657b9e7b6e8
Walther08 <gonzalezwalther08@gmail.com>
2021-12-09 12:38:18 -0500

    Cambios varios en el front

--- Frontend/src/app/modulos/administracion/personas/crear-persona/crear-persona.component.ts
+++ Frontend/src/app/modulos/administracion/personas/crear-persona/crear-persona.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import * as cryptoJS from 'crypto-js';
+import { ModeloPersona } from 'src/app/modelos/persona.modelo';
+import { AdministracionService } from 'src/app/servicios/administracion.service';
 
 @Component({
   selector: 'app-crear-persona',
@@ -7,9 +11,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CrearPersonaComponent implements OnInit {
 
-  constructor() { }
+  fgValidator : FormGroup = this.fb.group({
+    'nombres':['',[Validators.required]],
+    'apellidos':['',[Validators.required]],
+    'correo':['',[Validators.required,Validators.email]],
+    'celular':['',[Validators.required]],
+    'clave':['',[Validators.required]]
+  })
+
+  constructor(private fb:FormBuilder,
+    private servicioAdministracion: AdministracionService) { }
 
   ngOnInit(): void {
+    
+  }
+  
+  EnviarPersona(){
+    let nombresPersona = this.fgValidator.controls["nombres"].value;
+    let apellidosPersona = this.fgValidator.controls["apellidos"].value;
+    let correoPersona = this.fgValidator.controls["correo"].value;
+    let celularPersona = this.fgValidator.controls["celular"].value;
+    let clavePersona = this.fgValidator.controls["clave"].value;
+    //let claveCifrada = cryptoJS.MD5(clavePersona).toString();
+    let nuevaPersona = new ModeloPersona();
+    nuevaPersona.nombres = nombresPersona;
+    nuevaPersona.apellidos = apellidosPersona;
+    nuevaPersona.correo = correoPersona;
+    nuevaPersona.celular = celularPersona;
+    nuevaPersona.clave = clavePersona;
+    
+    this.servicioAdministracion.CrearPersona(nuevaPersona).subscribe((datos:ModeloPersona)=>{
+      alert("La persona fue creada correctamente");
+    },(error:any)=>{
+      alert("Error en el almacenamiento");
+    });
   }
 
 }
